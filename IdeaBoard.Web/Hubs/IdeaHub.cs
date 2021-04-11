f5a854a17bf6665f867d9523c83312372450bd25
emsenturk <emsenturk@cits.com.tr>
2021-04-11 14:39:19 +0300

    SignalR implemented.

---
+++ IdeaBoard.Web/Hubs/IdeaHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeaBoard.Web.Hubs
+{
+    public class IdeaHub : Hub
+    {
+        public async Task SaveIdea(string sessionId, string description)
+        {
+            //Tüm ideas alınacak burada.
+            await Clients.All.SendAsync("UpdateIdeas", sessionId, description);
+        }
+    }
+}
---
+++ IdeaBoard.Web/wwwroot/js/hubs/ideaHub.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/ideaHub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("UpdateIdeas", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    console.log(user, message);
+    connection.invoke("SaveIdea", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
